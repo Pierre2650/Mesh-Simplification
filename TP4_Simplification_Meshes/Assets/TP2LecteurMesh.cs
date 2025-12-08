50d12d29d4e22540d328cfbe805b96675d1201d8
Seraphiel <lein2650@gmail.com>
2025-12-08 13:05:12 +0100

    end

--- TP4_Simplification_Meshes/Assets/TP2LecteurMesh.cs
+++ TP4_Simplification_Meshes/Assets/TP2LecteurMesh.cs
@@ -4,6 +4,8 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 using Color = UnityEngine.Color;
 
 public class TP2Mesh : MonoBehaviour
@@ -13,17 +15,62 @@ public class TP2Mesh : MonoBehaviour
     public string fileName;
     public int nbTrianglesToRemove;
     public float size;
+    
+    public bool reduceTriangles;
+    public float tolerance;
 
     private List<int> triangles = new List<int>();
     private List<Vector3> vertices = null;
 
+   
     private class MyCube
     {
-       List<Vector3> points = new List<Vector3>();
+        public Vector3 v0;
+        public Vector3 v1;
+        public Vector3 v2;
+        public Vector3 v3;
+        public Vector3 v4;
+        public Vector3 v5;
+        public Vector3 v6;
+        public Vector3 v7;  
+
+        public List<Vector3> verticesInsideCube = new List<Vector3>();
+        public List<int> verticesIndexes = new List<int>();
+        public MyCube(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Vector3 v5, Vector3 v6, Vector3 v7)
+        {
+            this.v0 = v0;
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+            this.v7 = v7;
+        }
+
+        public bool vextexInsideCube( Vector3 Vertex)
+        {
+            // front face
+            if (Vertex.x < v0.x || Vertex.y < v0.y || Vertex.z  < v0.z) { return false; }
+            if (Vertex.x > v1.x || Vertex.y < v1.y || Vertex.z < v1.z) { return false; }
+            if (Vertex.x < v2.x || Vertex.y > v2.y || Vertex.z < v2.z) { return false; }
+            if (Vertex.x > v3.x || Vertex.y > v3.y || Vertex.z < v3.z) { return false; }
+
+            // back face
+            if (Vertex.x < v4.x || Vertex.y < v4.y || Vertex.z > v4.z) { return false; }
+            if (Vertex.x > v5.x || Vertex.y < v5.y || Vertex.z > v5.z) { return false; }
+            if (Vertex.x < v6.x || Vertex.y > v6.y || Vertex.z > v6.z) { return false; }
+            if (Vertex.x > v7.x || Vertex.y > v7.y || Vertex.z > v7.z) { return false; }
+            
+            return true;
+        }
     }
 
+
     private List<Vector3> grid = new List<Vector3>();
-    
+
+    private List<MyCube> grid2 = new List<MyCube>();
+
     private float maxX = 0, maxY = 0, maxZ = 0, minX = float.PositiveInfinity, minY = float.PositiveInfinity, minZ = float.PositiveInfinity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,8 +80,18 @@ public class TP2Mesh : MonoBehaviour
         myMF.mesh.Clear();
 
         readOFF();
-        //exportFile();
-        createGrid();
+
+        if (reduceTriangles)
+        {
+            createGrid();
+            calculateNewVertices();
+            cleanUnusedVertices();
+            myMF.mesh.Clear();
+            myMF.mesh.vertices = vertices.ToArray();
+            myMF.mesh.triangles = triangles.ToArray();
+        }
+   
+
     }
 
 
@@ -78,10 +135,6 @@ public class TP2Mesh : MonoBehaviour
 
         }
 
-       
-
-
-        vertices = tempVertices.ToList();
 
         center = new Vector3(center.x / nbVertices, center.y / nbVertices, center.z / nbVertices);
 
@@ -101,8 +154,7 @@ public class TP2Mesh : MonoBehaviour
             
         }
 
-        Debug.Log("Max = " + new Vector3(maxX,maxY,maxZ));
-       Debug.Log("Min = " + new Vector3(minX, minY,minZ));
+        vertices = tempVertices.ToList();
 
         Vector3[] trianglesNormals = new Vector3[nbTriangles];
         for (int i = 0; i < nbTriangles; lineIndex++, i++)
@@ -145,184 +197,138 @@ public class TP2Mesh : MonoBehaviour
 
 
 
-        myMF.mesh.vertices = tempVertices;
+        myMF.mesh.vertices = vertices.ToArray();
         myMF.mesh.triangles = triangles.ToArray();
         myMF.mesh.normals = verticesNormals;
 
     }
 
 
-    private void exportFile()
+     private void createGrid()
     {
-        int nbTriToRemove = nbTrianglesToRemove;
-        string path = "Assets\\";
-        path += fileName;
+        int nbVertexs = 0;
 
-
-        if (!File.Exists(path))
+        for (float z = minZ; z < maxZ  ; z += tolerance)
         {
-            Debug.LogError("File Doesn't exist in the assets folder");
-            return;
-        }
-
-        string[] lines = File.ReadAllLines(path);
-        string[] parts = lines[1].Split(' ');
-        int nbVertices = int.Parse(parts[0]);
-        int nbTriangles = int.Parse(parts[1]);
-
-        if (nbTriToRemove > nbTriangles)
-        {
-            Debug.LogError("number of triangles to remove is bigger than the amount of triangles in the file");
-            return;
-        }
-
-
-        int nbRemovedVertices = 0;
-        int j = 0, k = 0;
-        for (; j < nbTriToRemove; j++, k += 3)
-        {
-
-            for (int i = 0; i < vertices.Count; i++)
+            for (float y = minY; y < maxY ; y += tolerance)
             {
-                if (triangles[k] == i && !float.IsNegativeInfinity(vertices[i].x))
-                { vertices[i] = Vector3.negativeInfinity; nbRemovedVertices++; }
-
-                if (triangles[k + 1] == i && !float.IsNegativeInfinity(vertices[i].x))
-                { vertices[i] = Vector3.negativeInfinity; nbRemovedVertices++; }
-
-                if (triangles[k + 2] == i && !float.IsNegativeInfinity(vertices[i].x))
-                { vertices[i] = Vector3.negativeInfinity; nbRemovedVertices++; }
-
-            }
-
-            triangles[k] = -1;
-            triangles[k + 1] = -1;
-            triangles[k + 2] = -1;
-        }
-
-
-
-        for (; j < nbTriangles; j++, k += 3)
-        {
-
-            for (int i = 0; i < vertices.Count; i++)
-            {
-                if (triangles[k] == i && float.IsNegativeInfinity(vertices[i].x))
+                for (float x = minX; x < maxX ; x += tolerance)
                 {
-                    triangles[k] = -1;
-                    triangles[k + 1] = -1;
-                    triangles[k + 2] = -1;
+                      Vector3 v0 = new Vector3(x, y, z);
+                      Vector3 v1 = new Vector3(x + tolerance, y, z); 
 
-                    nbTriToRemove++;
-                    break;
+                      Vector3 v2 = new Vector3(x , y + tolerance, z);
+                      Vector3 v3 = new Vector3(x + tolerance, y + tolerance, z );
+
+                      Vector3 v4 = new Vector3(x, y, z + tolerance);
+                      Vector3 v5 = new Vector3(x + tolerance, y, z + tolerance);
+
+                      Vector3 v6 = new Vector3(x, y + tolerance, z + tolerance);
+                      Vector3 v7 = new Vector3(x + tolerance, y + tolerance, z + tolerance);
+
+                    MyCube temp = new MyCube(v0, v1, v2, v3, v4, v5, v6,v7);
+                    grid2.Add(temp);
+
+                   
+                }
+            }
+        }
+
+
+        foreach(MyCube aCube in grid2)
+        {
+            int i = 0;
+            foreach (Vector3 vertice in vertices)
+            {
+                if (aCube.vextexInsideCube(vertice))
+                {
+                    aCube.verticesInsideCube.Add(vertice);
+                    aCube.verticesIndexes.Add(i);
+
+                    nbVertexs++;
                 }
 
-                if (triangles[k + 1] == i && float.IsNegativeInfinity(vertices[i].x))
-                {
-                    triangles[k] = -1;
-                    triangles[k + 1] = -1;
-                    triangles[k + 2] = -1;
-
-                    nbTriToRemove++;
-                    break;
-                }
-
-                if (triangles[k + 2] == i && float.IsNegativeInfinity(vertices[i].x))
-                {
-                    triangles[k] = -1;
-                    triangles[k + 1] = -1;
-                    triangles[k + 2] = -1;
-
-                    nbTriToRemove++;
-                    break;
-                }
-
+                i++;
             }
 
+
         }
 
+        Debug.Log("nbVertexes = " +  nbVertexs);
+        Debug.Log("vertices.count() = " + vertices.Count());
 
+    }
 
-
-        if (nbVertices - nbRemovedVertices == 0)
+    private void calculateNewVertices()
+    {
+        foreach (MyCube aCube in grid2)
         {
+            if (aCube.verticesIndexes.Count == 0){ continue; }
 
-            triangles.Clear();
+            Vector3 averageVert = Vector3.zero;
 
-            nbVertices = 0;
-            nbTriangles = 0;
-        }
-        else
-        {
-            nbVertices -= nbRemovedVertices;
-            nbTriangles -= nbTriToRemove;
-        }
-
-        string[] newFile = new string[2 + nbVertices + nbTriangles];
-        newFile[0] = "OFF";
-        newFile[1] = nbVertices + " " + nbTriangles + " 0";
-
-
-        int nbRemoved = 0;
-        int verticeIndex = 0;
-        int lineIndex = 2;
-
-
-        foreach (Vector3 v in vertices)
-        {
-            if (float.IsNegativeInfinity(v.x))
+            for (int i = 0; i < aCube.verticesInsideCube.Count; i++)
             {
-                nbRemoved++;
+                averageVert += aCube.verticesInsideCube[i];
+                
             }
-            else
-            {
-                newFile[lineIndex] = v.x.ToString(CultureInfo.InvariantCulture) + " " + v.y.ToString(CultureInfo.InvariantCulture) + " " + v.z.ToString(CultureInfo.InvariantCulture);
-                lineIndex++;
 
-                for (int y = 0; y < triangles.Count; y++)
+            averageVert /= aCube.verticesInsideCube.Count();
+
+            vertices[aCube.verticesIndexes[0]] = averageVert;
+
+
+            for (int i = 1; i < aCube.verticesIndexes.Count; i++)
+            {
+                for(int j = 0; j < triangles.Count;  j++)
                 {
-                    if (triangles[y] == verticeIndex)
+                    if(triangles[j] == aCube.verticesIndexes[i])
                     {
-                        triangles[y] -= nbRemoved;
+                        triangles[j] = aCube.verticesIndexes[0];
                     }
                 }
+
             }
 
-            verticeIndex++;
+
         }
 
 
-        for (int y = 0; y < triangles.Count; y += 3)
-        {
-            if (triangles[y] != -1)
-            {
-                newFile[lineIndex] = "3 " + triangles[y] + " " + triangles[y + 1] + " " + triangles[y + 2];
-                lineIndex++;
-            }
-        }
 
-
-        string newPath = "Assets\\Export.off";
-        File.WriteAllLines(newPath, newFile);
 
 
     }
 
-    private void createGrid()
+
+    private void cleanUnusedVertices()
     {
-        float step = 0.05f;
-        for (float z = minZ; z < maxZ + step; z += step) {
-            for (float y = minY; y < maxY + step; y += step)
-            {
-                for (float x = minX; x < maxX + step; x+= step)
-                {
-                    grid.Add(new Vector3(x, y, z));
-                    //Debug.Log("new Vector3(x, y, z) = " + new Vector3(x, y, z));
+        Dictionary<int, int> remap = new Dictionary<int, int>();
+        List<Vector3> newVertices = new List<Vector3>();
+        List<int> newTriangles = new List<int>();
 
-                }
+        int nextIndex = 0;
+
+        for (int i = 0; i < triangles.Count; i++)
+        {
+            int oldIndex = triangles[i];
+
+            if (!remap.TryGetValue(oldIndex, out int newIndex))
+            {
+                newIndex = nextIndex++;
+                remap.Add(oldIndex, newIndex);
+                newVertices.Add(vertices[oldIndex]);
             }
+
+            newTriangles.Add(newIndex);
         }
+
+        vertices = newVertices;
+        triangles = newTriangles;
     }
+
+    
+
+
     private void OnDrawGizmos()
     {
         foreach (Vector3 v in grid) {
@@ -330,6 +336,19 @@ public class TP2Mesh : MonoBehaviour
             Gizmos.DrawSphere(v, 0.01f);   
         }
 
+        foreach (MyCube cb in grid2)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(cb.v0, 0.01f);
+            Gizmos.DrawSphere(cb.v1, 0.01f);
+            Gizmos.DrawSphere(cb.v2, 0.01f);
+            Gizmos.DrawSphere(cb.v3, 0.01f);
+
+            Gizmos.DrawSphere(cb.v4, 0.01f);
+            Gizmos.DrawSphere(cb.v5, 0.01f);
+            Gizmos.DrawSphere(cb.v6, 0.01f);
+            Gizmos.DrawSphere(cb.v7, 0.01f);
+        }
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(new Vector3(maxX,maxY,maxZ), 0.01f);   
 
